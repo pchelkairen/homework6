b856bc9aa549cbeb5544d364b1f3695ed3ae5826
Irina <pamir.08@mail.ru>
2022-10-27 17:24:16 +0300

    3

---
+++ 3/Program.cs
@@ -0,0 +1,11 @@
+﻿//Написать программу копирования массива
+int[] arr = { 11, 6, 1, 6, 5 };
+ 
+int[] copy = new int[arr.Length];
+    for (int i = 0; i < arr.Length; i++) 
+    {
+        copy[i] = arr[i];
+    }
+ 
+    Console.WriteLine(String.Join(", ", copy));        
+

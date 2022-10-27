06c0f1f3cde07be7d1505151357e397ee89c96ec
Irina <pamir.08@mail.ru>
2022-10-27 17:21:47 +0300

    2

---
+++ 2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать числа Фибоначчи
+Console.Write("Enter a number: ");
+var input = int.Parse(Console.ReadLine());
+Console.Write($"Fibonacci numbers up to {input}: ");
+var (first, second) = (0, 1);
+while (first <= input)
+{
+    Console.Write($"{first}, ");
+    (first, second) = (second, first + second);
+}

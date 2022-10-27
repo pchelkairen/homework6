e0651a7230657173fb78356e250764f5afddf4e9
Irina <pamir.08@mail.ru>
2022-10-27 21:05:57 +0300

    4

---
+++ 4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать программу масштабирования фигуры
+int[] arr = { 0, 0, 2, 0, 2, 2, 0, 2 };
+
+int[] copy = new int[arr.Length];
+
+Console.WriteLine("Введите коэффициент масштабирования: ");
+int k = int.Parse(Console.ReadLine() ?? "0");
+
+for (int i = 0; i < arr.Length; i++)
+{
+    copy[i] = arr[i] * k;
+}
+
+Console.WriteLine(String.Join(", ", copy));
+
